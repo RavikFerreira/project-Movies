d0d31af5b1bc8b090a5bcafc98d0199df9800afb
adalmando <adalmando.silva@dcx.ufpb.br>
2023-05-22 01:43:16 -0300

    mudanças tela login

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -4,8 +4,13 @@ import './login.css';
 export default function login() {
 return (
     <div className="login">
-        <h2>Entrar</h2>
-        <p>Olá, bem-vindo de volta á sua conta!</p>
+        <h2>Fazer login</h2>
+        <p>Olá, bem-vindo ao ReelsReview!</p>
+        <input type='text'></input>
+        <br/>
+        <input type='password'></input>
+        <br/>
+        <button id='botao'>login</button>
     </div>
 );
 }
