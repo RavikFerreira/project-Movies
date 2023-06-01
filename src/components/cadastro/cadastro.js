a1c6f9cb8998b4b03879403d89738247889a4640
RavikFerreira <ravikferreira.developer@gmail.com>
2023-06-01 20:42:22 -0300

    Implementando a tela de cadastro

--- src/components/cadastro/cadastro.js
+++ src/components/cadastro/cadastro.js
@@ -1,10 +1,22 @@
 import React from "react";
+import './cadastro.css';
 
 export default function Cadastro(){
     return(
-        <div className="cadastro">
-            <input id="email" type='text' placeholder='Email' required="required"></input>
-            <input id="senha" type='password' placeholder='Senha' required="required"></input>
+        <div className="backcadastro">
+            <div className="cadastro">
+                <h2>Fazer login</h2>
+                <p>Olá, bem-vindo ao ReelsReview!</p>
+                
+                <input id="nome" type='text' placeholder='Nome' required="required"></input>
+                <input id="sobrenome" type='text' placeholder='Sobrenome' required="required"></input>
+                <input id="email" type='text' placeholder='Email' required="required"></input>
+                <input id="confirmarEmail" type='text' placeholder='Confirmar email' required="required"></input>
+                <input id="senha" type='password' placeholder='Senha' required="required"></input>
+                
+                <button id='botao'>Login</button><br/>
+            </div>
+            
         </div>
         
     );
