b976b72365cdb488d90ba97d4cf5456176472145
RavikFerreira <ravikferreira.developer@gmail.com>
2023-06-01 17:40:13 -0300

    Melhora na tela de login

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -3,14 +3,16 @@ import './login.css';
 
 export default function login() {
 return (
-    <div className="login">
+    <div className="back">
         <h2>Fazer login</h2>
         <p>Olá, bem-vindo ao ReelsReview!</p>
-        <input class="input" type='text' placeholder='Email' required="required"></input>
-        <br/>
-        <input class="input" id="senha" type='password' placeholder='Senha' required="required"></input>
-        <br/>
-        <button id='botao'>Login</button>
+        
+        <div className='login'>
+            <input id="email" type='text' placeholder='Email' required="required"></input>
+            <input id="senha" type='password' placeholder='Senha' required="required"></input>
+            <button id='botao'>Login</button><br/>
+            <p>Não tem conta?<a href=''> Criar conta.</a></p>
+        </div>
     </div>
 );
 }
