7b966549836c17c88d65e930ef6eb63df9f1eb7d
RavikFerreira <ravikferreira.developer@gmail.com>
2023-06-01 22:39:16 -0300

    tela de cadastro finalizada

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -12,12 +12,12 @@ return (
         <p className='texto'><b>SEU SITE DE RESENHA DE FILMES</b></p>
         
         <div className='login'>
-            <h2>Fazer login</h2>
-            <p>Olá, bem-vindo ao ReelsReview!</p>
+            <h2 id='texto'>Fazer login</h2>
+            <p id='p'>Olá, bem-vindo ao ReelsReview!</p>
             <input id="email" type='text' placeholder='Email' required="required"></input>
             <input id="senha" type='password' placeholder='Senha' required="required"></input>
             <button id='botao'>Login</button><br/>
-            <p>Não tem conta? <Link to='/cadastro'>Criar conta.</Link></p>
+            <p id='p2'>Não tem conta? <Link to='/cadastro'>Criar conta.</Link></p>
             
         </div>
     </div>
--- src/components/cadastro/cadastro.js
+++ src/components/cadastro/cadastro.js
@@ -1,20 +1,22 @@
 import React from "react";
 import './cadastro.css';
+import { Link } from "react-router-dom";
 
 export default function Cadastro(){
     return(
         <div className="backcadastro">
             <div className="cadastro">
-                <h2>Fazer login</h2>
-                <p>Olá, bem-vindo ao ReelsReview!</p>
+                <h2 id="tituloc">Criar conta</h2>
+                <p id="pc">Por favor preencha os campos!</p>
                 
                 <input id="nome" type='text' placeholder='Nome' required="required"></input>
                 <input id="sobrenome" type='text' placeholder='Sobrenome' required="required"></input>
-                <input id="email" type='text' placeholder='Email' required="required"></input>
+                <input id="emailc" type='text' placeholder='Email' required="required"></input>
                 <input id="confirmarEmail" type='text' placeholder='Confirmar email' required="required"></input>
-                <input id="senha" type='password' placeholder='Senha' required="required"></input>
+                <input id="senhac" type='password' placeholder='Senha' required="required"></input>
                 
-                <button id='botao'>Login</button><br/>
+                <button id='cadastrar'>Cadastrar</button><br/>
+                <p id='pc2'>Já tem conta? <Link to={"/"}>Login.</Link></p>
             </div>
             
         </div>
